9a39fe7174d01a203a0e1174545df7dbb49cb346
Adrien Pessu <adrien@pessu.net>
2020-05-17 13:45:07 +0200

    feat: update workout

--- src/app/shared/service/steps.service.ts
+++ src/app/shared/service/steps.service.ts
@@ -15,16 +15,56 @@ export class StepsService {
         title: 'Planche'
       },
       {
-        duration: 2,
+        duration: 20,
         title: 'Repos'
       },
       {
-        duration: 3,
+        duration: 30,
         title: 'Planche côté gauche'
       },
       {
-        duration: 2,
+        duration: 20,
         title: 'Repos'
+      },
+      {
+        duration: 30,
+        title: 'Planche côté droit'
+      },
+      {
+        duration: 20,
+        title: 'Repos'
+      },
+      {
+        duration: 50,
+        title: 'Planche bras tendus'
+      },
+      {
+        duration: 20,
+        title: 'Repos'
+      },
+      {
+        duration: 30,
+        title: 'Planche bras tendus côté gauche'
+      },
+      {
+        duration: 20,
+        title: 'Repos'
+      },
+      {
+        duration: 30,
+        title: 'Planche bras tendu côté droit'
+      },
+      {
+        duration: 20,
+        title: 'Repos'
+      },
+      {
+        duration: 50,
+        title: 'Planche'
+      },
+      {
+        duration: 30,
+        title: 'Étirement 🐍'
       }
     ];
   }
